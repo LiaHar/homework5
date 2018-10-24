7976b5905222441134e16b4b5f0b0ab96226ea82
LiaHar <lia-ht@yandex.ru>
2018-10-25 00:28:07 +0400

    ss

--- homwork5.js
+++ homwork5.js
@@ -1,4 +1,5 @@
-const colorArray = ["red", "green", "yellow", "pink"];
+const colorArray = ["black", "green", "yellow", "purple"];
+const items = [0,1];
 const rand = function(num) {
     return Math.floor(Math.random() * num) + 1;
     };
@@ -7,55 +8,63 @@ const canvas = document.getElementById("canvas");
 const context = canvas.getContext('2d');
 canvas.height =500;
 canvas.width = 500;
-context.fillStyle = "red";
-context.fillRect(0, 0, canvas.width, canvas.height);
+//context.fillStyle = "yellow";
+// context.fillRect(0, 0, canvas.width, canvas.height);   //what are these doing?
 
 const createBoxes = function(count, canvasWidth, canvasHeight){
-
-    return [
-        {
-     x: rand(canvasWidth),
-     y: rand(canvasHeight),
+const base = [];
+    for(let i = 0; i< count; i++){
+        base[base.length]= {
+     x: rand(canvasWidth-30),
+     y: rand(canvasHeight-30),
      width: 30,
      height: 30,
-     xDelta: 31,
-     yDelta: 31,
+     xDelta:0.1,         //Math.round(Math.random()),
+     yDelta:  0.1,     //Math.round(Math.random()),
      color: colorArray[rand(colorArray.length) - 1],
      draw: function(){
-          context.fillStyle = "green";
-// console.log(this.x, this.y, this.width, this.height)
+          context.fillStyle = this.color;
          context.fillRect(this.x, this.y, this.width, this.height);
      },
      update: function(){
          this.x += this.xDelta;
          this.y += this.yDelta;
-
-     }
+         if(this.x + 30 > canvas.width || this.x < 0 ){
+            this.xDelta *= -1;
+            this.yDelta *= 1;
+        }
+        if(this.y + 30 > canvas.height || this.y < 0){
+            this.yDelta *= -1;
+        }
+         
+       
     }
-]
+    }
+
 } 
+return base;
+}
+const a = createBoxes(16, canvas.width, canvas.height);
 
-const a = createBoxes(2, canvas.width, canvas.height);
-
-const draw = function() {
-//     context.fillStyle = colorArray[rand(colorArray.length) - 1];
-//     context.fillRect(0,0, canvas.height, canvas.width);
-    for(let i = 0;i< a.length; i++) {
-        const obj =a[i];
-        obj.draw();
+const draw = function(boxes) {
+ 
+    for(let i = 0;i< boxes.length; i++) {
+        boxes[i].draw();
     }
 }
 
-const update = function() {
-    for(let i = 0; i< a.length; i++){
-        const obj = a[i];
-        obj.update();
+const update = function(boxes) {
+    for(let i = 0; i< boxes.length; i++){
+        boxes[i].update();
+        
     }
 }
 
 const loop = function() {
-    draw();
-    update();
+    context.fillStyle = "pink";
+    context.fillRect(0,0,canvas.width, canvas.height);
+    draw(a);
+    update(a);
     requestAnimationFrame(loop);
 }
 
