bf79a4d6f24e96554e89a544434520506380f670
LiaHar <lia-ht@yandex.ru>
2018-10-28 21:08:47 +0400

    final commit

--- homwork5.js
+++ homwork5.js
@@ -1,5 +1,5 @@
-const colorArray = ["black", "green", "yellow", "purple"];
-const items = [0,1];
+const colorArray = ["black", "green", "yellow", "pink"];
+const colorArray1 = ["grey", "purple", "blue", "white"];
 const rand = function(num) {
     return Math.floor(Math.random() * num) + 1;
     };
@@ -8,19 +8,20 @@ const canvas = document.getElementById("canvas");
 const context = canvas.getContext('2d');
 canvas.height =500;
 canvas.width = 500;
-//context.fillStyle = "yellow";
-// context.fillRect(0, 0, canvas.width, canvas.height);   //what are these doing?
+context.fillStyle = "red";
+context.fillRect(0, 0, canvas.width, canvas.height);
 
 const createBoxes = function(count, canvasWidth, canvasHeight){
-const base = [];
-    for(let i = 0; i< count; i++){
-        base[base.length]= {
-     x: rand(canvasWidth-30),
-     y: rand(canvasHeight-30),
+const boxes = [];
+for(let i = 0; i < count; i++){
+    boxes[boxes.length] =
+        {
+     x: rand(canvasWidth - 30),
+     y: rand(canvasHeight - 30), 
      width: 30,
      height: 30,
-     xDelta:0.1,         //Math.round(Math.random()),
-     yDelta:  0.1,     //Math.round(Math.random()),
+     xDelta: 6,
+     yDelta: 6,
      color: colorArray[rand(colorArray.length) - 1],
      draw: function(){
           context.fillStyle = this.color;
@@ -29,42 +30,42 @@ const base = [];
      update: function(){
          this.x += this.xDelta;
          this.y += this.yDelta;
-         if(this.x + 30 > canvas.width || this.x < 0 ){
+         if(this.x + this.width> canvasWidth || this.x  < 0) {
+            this.color = colorArray1[rand(colorArray1.length) - 1];
             this.xDelta *= -1;
-            this.yDelta *= 1;
-        }
-        if(this.y + 30 > canvas.height || this.y < 0){
             this.yDelta *= -1;
-        }
-         
-       
-    }
-    }
+         }
+         if(this.y + this.height> canvasHeight || this.y  < 0){
+             this.color = colorArray[rand(colorArray.length) - 1];
+             this.xDelta *= 1;
+             this.yDelta *= -1;
+         }
 
-} 
-return base;
-}
-const a = createBoxes(16, canvas.width, canvas.height);
-
-const draw = function(boxes) {
- 
-    for(let i = 0;i< boxes.length; i++) {
-        boxes[i].draw();
+     }
     }
 }
+return boxes;
+ }
 
-const update = function(boxes) {
-    for(let i = 0; i< boxes.length; i++){
-        boxes[i].update();
-        
+const manyBoxes = createBoxes(30, canvas.width, canvas.height);
+
+const draw = function() {
+   for(let i = 0;i< manyBoxes.length; i++) {
+        manyBoxes[i].draw();
+    }
+}
+
+const update = function() {
+    for(let i = 0; i< manyBoxes.length; i++){
+        manyBoxes[i].update();
     }
 }
 
 const loop = function() {
-    context.fillStyle = "pink";
-    context.fillRect(0,0,canvas.width, canvas.height);
-    draw(a);
-    update(a);
+    context.fillStyle = "red";
+    context.fillRect(0,0, canvas.width, canvas.height);
+    draw();
+    update();
     requestAnimationFrame(loop);
 }
 
